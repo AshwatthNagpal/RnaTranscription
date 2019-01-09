441cc1e3e9fa225c2bdff621996c0635a338c5e3
Ashwatth Nagpal <ashwatthnagpal21@gmail.com>
2019-01-10 00:15:38 +0530

    Add dna_to_rna

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,4 +1,21 @@
-def hello
-  "hello world"
+def dna_to_rna(dna="")
+   if dna.empty?
+    return "empty"
+    end
+     rna=""
+    dna.each_char do |i|
+     if(i=="G")
+       rna+="C"
+     elsif i=="C"
+       rna+="G"
+     elsif i== "A"
+       rna+="U"
+     elsif i=="T"
+       rna+="A"
+     else
+       return "Wrong"
+     end
+    end
+   rna
 end
-puts hello
+puts dna_to_rna("GGTTATCC")
