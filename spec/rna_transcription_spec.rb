57b7bb1b47f4fea48e125778d808a76b39af548d
Ashwatth Nagpal <ashwatthnagpal21@gmail.com>
2019-01-10 17:10:20 +0530

    update comment

--- spec/rna_transcription_spec.rb
+++ spec/rna_transcription_spec.rb
@@ -1,10 +1,10 @@
-require  "rna_transcription.rb"
+require "rna_transcription.rb"
 
 describe "dna_to_rna" do
   it "empty string test" do
     expect(dna_to_rna("")).to eq "empty"
   end
-  it "G" do # change 2
+  it "G" do # change 2345
     expect(dna_to_rna("G")).to eq "C"
   end
   it "Rna complement of cytosine is guanine " do
@@ -16,7 +16,7 @@ describe "dna_to_rna" do
   it "T -> A" do
     expect(dna_to_rna("T")).to eq "A"
   end
-it "GGCCAT" do
-  expect(dna_to_rna("GGCAT")).to eq "CCGUA"
-end
+  it "GGCCAT" do
+    expect(dna_to_rna("GGCAT")).to eq "CCGUA"
+  end
 end
