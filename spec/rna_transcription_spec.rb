b2e87bdf6d4a29a1701d683e7c8f8475fe666019
Ashwatth Nagpal <ashwatthnagpal21@gmail.com>
2019-01-10 13:16:54 +0530

    added rspec test case file

--- spec/rna_transcription_spec.rb
+++ spec/rna_transcription_spec.rb
@@ -1,22 +1,22 @@
-require  "../rna_transcription.rb"
+require  "rna_transcription.rb"
 
 describe "dna_to_rna" do
   it "empty string test" do
-    expect(dna_to_rna("")).to eq"empty"
+    expect(dna_to_rna("")).to eq "empty"
   end
   it "G -> C" do
-    expect(dna_to_rna("G")).to eq"C"
+    expect(dna_to_rna("G")).to eq "C"
   end
   it "C -> G" do
-    expect(dna_to_rna("C")).to eq"G"
+    expect(dna_to_rna("C")).to eq "G"
   end
   it "A -> U" do
-    expect(dna_to_rna("A")).to eq"U"
+    expect(dna_to_rna("A")).to eq "U"
   end
   it "T -> A" do
-    expect(dna_to_rna("T")).to eq"A"
+    expect(dna_to_rna("T")).to eq "A"
   end
 it "GGCCAT" do
-  expect(dna_to_rna(GGCAT)).to eq"CCGGUA"
+  expect(dna_to_rna("GGCAT")).to eq "CCGUA"
 end
 end
